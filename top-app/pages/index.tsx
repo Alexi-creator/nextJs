a07dbd288ce07d9231c77c2b8a28bce78cfa55a5
ilya Pavlov <ilya.p@finanso.com>
2022-06-09 13:45:30 +0300

    add button mod

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home(): JSX.Element {
   return (
     <div>
-      <Htag tag='h1'>Text</Htag>
+      <Htag tag="h1">Text</Htag>
+      <Button appearance="primary" className="new class">Button</Button>
+      <Button appearance="ghost" arrow="right">Button</Button>
     </div>
   );
 }

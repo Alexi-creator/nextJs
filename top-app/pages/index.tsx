5d3fc6ea64b78cb1e0f5af49d710bc5b98ea96fe
ilya Pavlov <ilya.p@finanso.com>
2022-06-11 14:28:10 +0300

    add lint hooks

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Htag, Ptag, Tag } from "../components";
 
 export default function Home(): JSX.Element {
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    console.log('counter', counter);
+    return function cleanup() {
+      console.log('unmount');
+    };
+  }, []);
+
   return (
     <div>
+      {counter}
       <Htag tag="h1">Text</Htag>
-      <Button appearance="primary" arrow="down" className="new class">
-        Button
+      <Button
+        appearance="primary"
+        arrow="down"
+        className="new class"
+        onClick={() => setCounter((x) => x + 1)}
+      >
+        Button Counter
       </Button>
       <Button appearance="primary" className="new class">
         Button
@@ -16,7 +31,9 @@ export default function Home(): JSX.Element {
       </Button>
       <Ptag size="m">Параграф</Ptag>
       <Tag>Tag</Tag>
-      <Tag href="www.site.com" color="red">Tag2</Tag>
+      <Tag href="www.site.com" color="red">
+        Tag2
+      </Tag>
     </div>
   );
 }

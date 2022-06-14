a9374c26ffb91316dcbbc7e9e3d02a15cbcf03b9
ilya Pavlov <ilya.p@finanso.com>
2022-06-14 13:23:49 +0300

    footer done style and html

--- top-app/layout/Footer/Footer.tsx
+++ top-app/layout/Footer/Footer.tsx
@@ -1,6 +1,20 @@
 import { FooterProps } from "./Footer.props";
 import styles from "./Footer.module.css";
+import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-  return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+  return (
+    <footer className={cn(className, styles.footer)} {...props}>
+      <div className={styles.copyright}>
+        OwlTop © 2020 - {format(new Date, 'yyyy')} Все права защищены
+      </div>
+      <a href="#" target="_blank">
+        Пользовательское соглашение
+      </a>
+      <a href="#" target="_blank">
+        Политика конфиденциальности
+      </a>
+    </footer>
+  );
 };

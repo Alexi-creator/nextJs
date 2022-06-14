b5d9c21f88cf966aaf39dd26cde78811c9bda657
ilya Pavlov <ilya.p@finanso.com>
2022-06-14 15:26:32 +0300

    add axios, get data menu

--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,8 +1,11 @@
+import { GetStaticProps } from "next";
 import React, { useState } from "react";
 import { Button, Htag, Ptag, Tag, Rating } from "../components";
 import { withLayout } from "../layout/Layout";
+import axios from 'axios';
+import { MenuItem } from "../interfaces/menu.interface";
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
   const [counter, setCounter] = useState<number>(0);
   
   const [rating, setRating] = useState<number>(4);
@@ -31,8 +34,34 @@ function Home(): JSX.Element {
       <Tag href="www.site.com" color="red">
         Tag2
       </Tag>
+      <ul>
+        {menu.map((m) => (
+          <li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+        ))}
+      </ul>
     </>
   );
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const firstCategory = 0;
+  const { data: menu } = await axios.post<MenuItem[]>(
+    process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+    {
+      firstCategory,
+    }
+  );
+  return {
+    props: {
+      menu,
+      firstCategory
+    }
+  };
+};
+
+interface HomeProps extends Record<string, unknown> {
+  menu: MenuItem[];
+  firstCategory: number;
+}

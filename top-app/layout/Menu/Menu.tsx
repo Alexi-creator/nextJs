940ed8141c872572c56e1816397a83137a0fcaf5
ilya Pavlov <ilya.p@finanso.com>
2022-06-25 14:29:37 +0300

    add framer motion on menu

--- top-app/layout/Menu/Menu.tsx
+++ top-app/layout/Menu/Menu.tsx
@@ -6,10 +6,30 @@ import cn from "classnames";
 import Link from 'next/link';
 import { useRouter } from "next/router";
 import { firstLevelMenu } from "../../helpers/helpres";
+import { motion } from "framer-motion";
 
 export const Menu = (): JSX.Element => {
     const { menu, setMenu, firstCategory} = useContext(AppContext);
     const router = useRouter();
+
+    const variants = {
+      visiable: {
+        marginBottom: 20,
+        transition: {
+          when: 'beforeChildren',
+          staggerChildren: 0.1
+        }
+      },
+      hidden: {marginBottom: 0}
+    };
+
+    const variantsChildren = {
+      visiable: {
+        opacity: 1,
+        height: 29,
+      },
+      hidden: { opacity: 0, height: 0 },
+    };
 
     const openSecondLevel = (secondCategory: string) => {
         setMenu &&
@@ -63,13 +83,15 @@ export const Menu = (): JSX.Element => {
                         >
                           {m._id.secondCategory}
                         </div>
-                        <div
-                          className={cn(styles.secondLevelBlock, {
-                            [styles.secondLevelBlockOpened]: m.isOpened,
-                          })}
+                        <motion.div
+                          layout
+                          variants={variants}
+                          initial={m.isOpened ? "visiable" : "hidden"}
+                          animate={m.isOpened ? "visiable" : "hidden"}
+                          className={cn(styles.secondLevelBlock)}
                         >
                           {buildThirdLevel(m.pages, menuItem.route)}
-                        </div>
+                        </motion.div>
                       </div>
                     );
                 })}
@@ -80,15 +102,18 @@ export const Menu = (): JSX.Element => {
 
     const buildThirdLevel = (pages: PageItem[], route: string) => {
         return pages.map((p) => (
-          <Link href={`/${route}/${p.alias}`} key={p.alias}>
-            <a
-              className={cn(styles.thirdLevel, {
-                [styles.thirdLevelActive]: `/${route}/${p.alias}` === router.asPath
-              })}
-            >
-              {p.category}
-            </a>
-          </Link>
+          <motion.div key={p.alias} variants={variantsChildren}>
+            <Link href={`/${route}/${p.alias}`}>
+              <a
+                className={cn(styles.thirdLevel, {
+                  [styles.thirdLevelActive]:
+                    `/${route}/${p.alias}` === router.asPath,
+                })}
+              >
+                {p.category}
+              </a>
+            </Link>
+          </motion.div>
         ));
     };
 
